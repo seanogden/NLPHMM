3ad4a97936d63179ea555fba5609934481c7a824
JINGXIAPANG <pangjac@dhcp-ccc-3128.redrover.cornell.edu>
2016-03-19 13:14:01 -0400

    sean is smart

--- src/edu/cornell/cs/nlp/assignments/POSTaggerTester.java
+++ src/edu/cornell/cs/nlp/assignments/POSTaggerTester.java
@@ -577,6 +577,7 @@ public class POSTaggerTester {
 		public void train(
 				List<LabeledLocalTrigramContext> labeledLocalTrigramContexts) {
 			// collect word-tag counts
+			// I like Sean, he is smart!!!
 			for (final LabeledLocalTrigramContext labeledLocalTrigramContext : labeledLocalTrigramContexts) {
 				final String word = labeledLocalTrigramContext.getCurrentWord();
 				final String tag = labeledLocalTrigramContext.getCurrentTag();
